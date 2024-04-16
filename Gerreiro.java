eaef6e45eb4c96291b1e6062dd434a6f88964228
Fernando pereira <fernandomendespereira@hotmail.com>
2024-04-16 20:08:14 -0300

    iniciando aplicaçao

---
+++ Gerreiro.java
@@ -0,0 +1,15 @@
+public class Gerreiro extends Personagem {
+    private int pontosForca;
+
+    public int getPontosForca() {
+        return pontosForca;
+    }
+
+    public void setPontosForca(int pontosForca) {
+        this.pontosForca = pontosForca;
+    }
+
+    public Gerreiro(int pontosForca) {
+        this.pontosForca = pontosForca;
+    }
+}
